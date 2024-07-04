a24ba79f3180c2708945f6858a1c542cc07d26db
rg345 <142898336+rg345@users.noreply.github.com>
2024-07-04 17:58:48 +0000

    variables and datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 2345
+let name = "rohit"
+var orderId = "34"
+accountName= "rgunwal"
+var address
+
+console.log(accountId)
+console.table([accountId,name, orderId,accountName,address])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"
+// treat all js code as newer version
+
+// alert(2+2) it doesnt work, we are using nodejs not browser
+
+//number
+// bigint
+//string,boolean
+// null = standalone value 
+// undefined matlab apne koi var banaya aur use declare nhi kiya
+// null matlab empty string ans = null ise aap assign bhi kar sakte hai
+
+//symbol = unique (will be used in react heavily)
+
+console.log(typeof"rohit")
+
+console.log(typeof null)
+// type of null is an object
+
+console.log(typeof undefined)
+// type of undefined is undefined
