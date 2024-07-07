ba8805e8918414a5426dd8820ddef3ccabedcbe7
rg345 <142898336+rg345@users.noreply.github.com>
2024-07-07 10:02:20 +0000

    date time done

---
+++ 01_basics/07_datetime.js
@@ -0,0 +1,12 @@
+// const date = new Date();
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+// console.log(date.toDateString())
+
+// console.log(date.toISOString())
+let newDate = new Date(2024, 0,14);
+console.log(newDate.toDateString())
+//0 is the first month in javascript
+
+let mytimeStamp  = Date.now()
+console.log(mytimeStamp)
